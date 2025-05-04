ac3c07403986b19349351282af478701c1984a20
Karl Solbue Vika / NIFU <karl.solbue.vika@nifu.no>
2025-05-04 17:14:36 +0200

    Legg til zzz.R med globalVariables for å unngå NOTE i R CMD check

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c(
+  "Andel", "Standardfeil", "Andel (%)", "Nedre grense (95 % CI)", "Øvre grense (95 % CI)",
+  "parameter", "type", "verdi", "verdi_navn", "variabel", "Variabel", "variabelnavn",
+  "Gjennomsnitt", "kategori", "n", "Freq", "stat", "se", "p", "p_adj", "ci_low", "ci_high",
+  "sammenlikning"
+))
